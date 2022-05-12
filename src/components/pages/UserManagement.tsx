5622e09190c428996c733b5dddff1b622302ce0d
kotomi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-05-12 17:35:07 +0900

    ユーザーカードデザイン適用

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,35 @@
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理画面です</p>;
+  return (
+    <Wrap spacing="30px">
+      <WrapItem>
+        <Box
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+        >
+          <Stack textAlign="center">
+            <Image
+              borderRadius="full"
+              boxSize="160px"
+              src="https://source.unsplash.com/random"
+              alt="プロフィール画像"
+              m="auto"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              ぴーつん
+            </Text>
+            <Text fontSize="sm" color="gray">
+              ぱーつん
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
