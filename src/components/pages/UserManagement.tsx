7900bb0e4bd20d87fccb71e34253c64fc8427b78
kotomi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-05-17 22:04:25 +0900

    ユーザー一覧画面作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,34 +1,17 @@
-import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap spacing="30px">
+    <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="10px"
-          shadow="md"
-          p={4}
-        >
-          <Stack textAlign="center">
-            <Image
-              borderRadius="full"
-              boxSize="160px"
-              src="https://source.unsplash.com/random"
-              alt="プロフィール画像"
-              m="auto"
-            />
-            <Text fontSize="lg" fontWeight="bold">
-              ぴーつん
-            </Text>
-            <Text fontSize="sm" color="gray">
-              ぱーつん
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="ぴーつん"
+          fullName="左足の方がでかい"
+        />
       </WrapItem>
     </Wrap>
   );
